615455ae7bb9a2564fc1092290e6ff2ab76985ca
Niels Clarysse <niels.clarysse@student.howest.be>
2026-02-27 15:57:37 +0100

    Exercises 1 - 4: Using the delegate keyword

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System;
+
+/* ******************************************* */
+/* Exercises 1 - 4: Using the delegate keyword */
+/* ******************************************* */
+
+namespace ConsoleApp
+{
+    delegate string CapitalizeDel(string s);
+    delegate int CountDel(string s);
+    delegate bool CompareDel(string s1, string s2);
+    delegate string SortDel(string s);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CapitalizeDel capitalize = (s) => s.ToUpper();
+            Console.WriteLine(capitalize("hello world"));
+
+            CountDel countChars = (s) => s.Length;
+            Console.WriteLine(countChars("hello world"));
+
+            CompareDel isLongerThan = (s1, s2) => s1.Length > s2.Length;
+            Console.WriteLine(isLongerThan("hello", "world"));
+
+            SortDel sortString = (s) => {
+                char[] chars = s.ToCharArray();
+                Array.Sort(chars);
+                return new string(chars);
+            };
+            Console.WriteLine(sortString("hello world"));
+        }
+    }
+}
--- ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b274dfc0ab6dc13be59f536cd82179e146fd78e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
